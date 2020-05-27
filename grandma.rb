5d425611f072bffd4ab2c8af160b3a304402544f
Tanya Hodges <tanyac52003@gmail.com>
2020-05-27 01:47:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
     elsif phrase != phrase.upcase
-    "HHU?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   else
     "NO, NOT SINCE 1938!"
   end 
